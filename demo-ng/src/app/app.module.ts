f2374756a0de334c415ad04cac0d4e3b8e1de77d
Jonathan Salomon <mail2joni@gmail.com>
2020-06-08 00:28:42 +0300

    Added demo-ng project

--- demo-ng/src/app/app.module.ts
+++ demo-ng/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptModule } from "@nativescript/angular/nativescript.module";
+import { SocketIOModule } from 'nativescript-socketio/angular';
+import { WebRTCModule } from "nativescript-webrtc/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+
+import { server } from './webrtc/environment';
+import { WebRTCProvider } from "./webrtc/webrtc.provider";
+import { WebRTCComponent } from "./webrtc/webrtc.component";
+
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -13,24 +18,29 @@ import { ItemDetailComponent } from "./item/item-detail.component";
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
-    imports: [
-        NativeScriptModule,
-        AppRoutingModule
-    ],
-    declarations: [
-        AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
-    ],
-    providers: [],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  bootstrap: [
+    AppComponent
+  ],
+  imports: [
+    NativeScriptModule,
+    AppRoutingModule,
+    SocketIOModule.forRoot(server),
+    WebRTCModule,
+  ],
+  declarations: [
+    AppComponent,
+    WebRTCComponent,
+  ],
+  entryComponents: [
+  ],
+  providers: [
+    WebRTCProvider,
+  ],
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ]
 })
 /*
 Pass your application module to the bootstrapModule function located in main.ts to start your app
 */
-export class AppModule { }
+export class AppModule {}
